be07decd2887be86860735cbe9012031b5e09ee0
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-18 13:21:47 +0100

    get all and id methods added to customer

---
+++ src/main/java/com/courseBooking/controllers/CustomerController.java
@@ -0,0 +1,26 @@
+package com.courseBooking.controllers;
+
+import com.courseBooking.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity getAllCustomers() {
+        return new ResponseEntity(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/customers/{id}")
+    public ResponseEntity getCustomer(@PathVariable Long id) {
+        return new ResponseEntity(customerRepository.findById(id), HttpStatus.OK);
+    }
+}
