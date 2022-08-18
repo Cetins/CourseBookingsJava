5c52e63fd881512842861906cefced8c625a5a4f
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-18 12:45:24 +0100

    getters and setters added to all

--- src/main/java/com/courseBooking/models/Customer.java
+++ src/main/java/com/courseBooking/models/Customer.java
@@ -33,4 +33,44 @@ public class Customer {
         this.age = age;
         this.bookings = new ArrayList<>();
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
