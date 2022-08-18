31475acb6c670c91d167520ece1e6199c25db7eb
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-18 12:01:30 +0100

    star enums added, course basics added

---
+++ src/main/java/com/courseBooking/models/Course.java
@@ -0,0 +1,23 @@
+package com.courseBooking.models;
+
+import com.courseBooking.Star;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "town")
+    private String town;
+
+    @Column(name = "star")
+    private Star star;
+}
