e8ad4ece986c7c10017e09e64e6ee592080433f7
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-18 13:16:47 +0100

    get all and id methods added to course

--- src/main/java/com/courseBooking/controllers/CourseController.java
+++ src/main/java/com/courseBooking/controllers/CourseController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,10 @@ public class CourseController {
     @GetMapping(value = "/courses")
     public ResponseEntity getAllCourses() {
         return new ResponseEntity(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/courses/{id}")
+    public ResponseEntity getCourse(@PathVariable Long id) {
+        return new ResponseEntity(courseRepository.findById(id), HttpStatus.OK);
     }
 }
