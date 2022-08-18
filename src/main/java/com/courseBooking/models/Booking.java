9b88a65c7028ca5c6fd2625375bd5cc1afe96845
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-18 12:04:36 +0100

    booking basics added

---
+++ src/main/java/com/courseBooking/models/Booking.java
@@ -0,0 +1,15 @@
+package com.courseBooking.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    private String date;
+}
