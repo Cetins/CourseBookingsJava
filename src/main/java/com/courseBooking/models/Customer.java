901bd1492d84f79d0cfb4d51ec08a5b50c9ebd99
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-18 12:07:09 +0100

    customer basics added

---
+++ src/main/java/com/courseBooking/models/Customer.java
@@ -0,0 +1,21 @@
+package com.courseBooking.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "town")
+    private String town;
+
+    @Column(name = "age")
+    private Integer age;
+}
