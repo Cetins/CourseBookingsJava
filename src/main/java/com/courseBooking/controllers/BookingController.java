87d972febe65570c258f912e41851202a6289f5e
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-18 14:52:12 +0100

    customers by age town and course added

--- src/main/java/com/courseBooking/controllers/BookingController.java
+++ src/main/java/com/courseBooking/controllers/BookingController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,12 @@ public class BookingController {
     BookingRepository bookingRepository;
 
     @GetMapping(value = "/bookings")
-    public ResponseEntity getAllBookings() {
+    public ResponseEntity getAllBookings(
+            @RequestParam(required = false, name = "date") String date
+    ) {
+        if (date != null) {
+            return new ResponseEntity(bookingRepository.findByDate(date), HttpStatus.OK);
+        }
         return new ResponseEntity(bookingRepository.findAll(), HttpStatus.OK);
     }
 
--- src/main/java/com/courseBooking/repositories/CustomerRepository.java
+++ src/main/java/com/courseBooking/repositories/CustomerRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     List<Customer> findByBookingsCourseNameIgnoreCase(String name);
+    List<Customer> findByTownIgnoreCaseAndBookingsCourseNameIgnoreCase(String town, String name);
+    List<Customer> findByAgeGreaterThanAndTownIgnoreCaseAndBookingsCourseNameIgnoreCase(Integer age, String town, String name);
 }
